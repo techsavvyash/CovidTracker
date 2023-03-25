be9fe3e1fdaf68a2df8379c5d47bc37e209ed094
Yash Mittal <mittal.yash12@hotmail.com>
2023-03-25 17:08:58 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express() ;
+
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+})
+
+app.get('/style.css', (req, res) => {
+  res.sendFile(__dirname + '/style.css');
+});
+
+
+app.listen(3000, () => {
+  console.log('Server is listening on port 3000, http://localhost:3000');
+})
+
